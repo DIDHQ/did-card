d5405cdf15a313e1db628a3544dfdb4ee64201e4
Aliez <renzholy@gmail.com>
2023-08-16 18:50:08 +0900

    Update use-related-addresses.ts

--- src/hooks/use-related-addresses.ts
+++ src/hooks/use-related-addresses.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr'
 import { getRelatedAddresses } from '@/utils/did'
 
 export default function useRelatedAddresses(did: string) {
-  did = did.replace(/\W/g, '')
+  did = did.replace(/\s/g, '')
   return useSWR(did ? ['related', did] : null, () => getRelatedAddresses(did), {
     revalidateOnFocus: false,
   })
