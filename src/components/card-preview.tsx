f9961d1d5fb662ca06ca5fec88be650c8891c405
Aliez <renzholy@gmail.com>
2023-08-21 15:05:45 +0900

    print

--- src/components/card-preview.tsx
+++ src/components/card-preview.tsx
@@ -3,6 +3,7 @@ import useSWRMutation from 'swr/mutation'
 import { wrap } from 'comlink'
 import useSWR from 'swr'
 import { useCallback, useRef } from 'react'
+import { createPortal } from 'react-dom'
 import DidCard from './did-card'
 import { DownloadIcon, LoadingIcon, PrintIcon, UploadIcon } from './icon'
 import ParallaxStars from './parallax-stars'
@@ -32,20 +33,24 @@ export default function CardPreview(props: {
     { revalidateOnFocus: false },
   )
   const inputRef = useRef<HTMLInputElement>(null)
-  const { trigger, isMutating } = useSWRMutation('download', async () => {
-    if (!svg) {
-      return
-    }
+  const iframeRef = useRef<HTMLIFrameElement>(null)
+  const { trigger: download, isMutating: isDownloading } = useSWRMutation(
+    'download',
+    async () => {
+      if (!svg) {
+        return
+      }
 
-    const href = await convert.call(convert, svg)
-    const anchor = document.createElement('a')
-    anchor.setAttribute('download', `${props.did}.png`)
-    anchor.href = href
-    anchor.setAttribute('target', '_blank')
-    anchor.click()
+      const href = await convert.call(convert, svg)
+      const anchor = document.createElement('a')
+      anchor.setAttribute('download', `${props.did}.png`)
+      anchor.href = href
+      anchor.setAttribute('target', '_blank')
+      anchor.click()
 
-    URL.revokeObjectURL(href)
-  })
+      URL.revokeObjectURL(href)
+    },
+  )
   const handleFile = useCallback(
     (file?: File) => {
       if (!file) {
@@ -60,67 +65,107 @@ export default function CardPreview(props: {
     },
     [onDidChange, onImageChange],
   )
+  const { trigger: print, isMutating: isPrinting } = useSWRMutation(
+    'print',
+    async () => {
+      const iframe = iframeRef.current
+      if (!svg || !iframe) {
+        return
+      }
+
+      const href = await convert.call(convert, svg)
+      iframe.onload = () => {
+        const image = document.createElement('img')
+        image.onload = () => {
+          iframe.contentWindow?.print()
+          URL.revokeObjectURL(href)
+        }
+        image.src = href
+        iframe.contentDocument?.body.appendChild(image)
+      }
+      iframe.setAttribute(
+        'srcdoc',
+        `<html><body style="margin: 0;"></body></html>`,
+      )
+    },
+  )
 
   return (
-    <div
-      onDragOver={(e) => {
-        e.preventDefault()
-      }}
-      onDragEnd={(e) => {
-        e.preventDefault()
-      }}
-      onDrop={(e) => {
-        e.preventDefault()
-        handleFile(e.nativeEvent.dataTransfer?.files[0])
-      }}
-      className={clsx('relative', props.className)}
-    >
-      <ParallaxStars
-        stars={100}
-        speed={0.3}
-        color="#ffffff"
-        className="h-full w-full bg-gradient"
-      />
+    <>
       <div
-        className={clsx(
-          'absolute inset-0 flex h-full w-full flex-col items-center justify-center',
-          props.className,
-        )}
+        onDragOver={(e) => {
+          e.preventDefault()
+        }}
+        onDragEnd={(e) => {
+          e.preventDefault()
+        }}
+        onDrop={(e) => {
+          e.preventDefault()
+          handleFile(e.nativeEvent.dataTransfer?.files[0])
+        }}
+        className={clsx('relative', props.className)}
       >
-        <DidCard svg={svg} />
-        <div className="flex gap-8">
-          <button
-            onClick={() => inputRef.current?.click()}
-            className="mt-16 rounded-full bg-white p-2 font-semibold leading-4 shadow-2xl transition-colors hover:bg-gray-300 disabled:cursor-wait"
-          >
-            <input
-              ref={inputRef}
-              type="file"
-              accept="image/png,image/jpg,image/jpeg,image/gif"
-              onChange={(e) => handleFile(e.target.files?.[0])}
-              className="hidden"
-            />
-            <UploadIcon className="h-8 w-8 text-gray-800" />
-          </button>
-          <button
-            disabled={isMutating}
-            onClick={() => trigger()}
-            className="mt-16 rounded-full bg-white p-2 font-semibold leading-4 shadow-2xl transition-colors hover:bg-gray-300 disabled:cursor-wait"
-          >
-            {isMutating ? (
-              <LoadingIcon className="h-8 w-8 text-gray-400" />
-            ) : (
-              <DownloadIcon className="h-8 w-8 text-gray-800" />
-            )}
-          </button>
-          {/* <button
-            onClick={() => {}}
-            className="mt-16 rounded-full bg-white p-2 font-semibold leading-4 shadow-2xl transition-colors hover:bg-gray-300 disabled:cursor-wait"
-          >
-            <PrintIcon className="h-8 w-8 text-gray-800" />
-          </button> */}
+        <ParallaxStars
+          stars={100}
+          speed={0.3}
+          color="#ffffff"
+          className="h-full w-full bg-gradient"
+        />
+        <div
+          className={clsx(
+            'absolute inset-0 flex h-full w-full flex-col items-center justify-center',
+            props.className,
+          )}
+        >
+          <DidCard svg={svg} />
+          <div className="flex gap-8">
+            <button
+              onClick={() => inputRef.current?.click()}
+              className="mt-16 rounded-full bg-white p-2 font-semibold leading-4 shadow-2xl transition-colors hover:bg-gray-300 disabled:cursor-wait"
+            >
+              <input
+                ref={inputRef}
+                type="file"
+                accept="image/png,image/jpg,image/jpeg,image/gif"
+                onChange={(e) => handleFile(e.target.files?.[0])}
+                className="hidden"
+              />
+              <UploadIcon className="h-8 w-8 text-gray-800" />
+            </button>
+            <button
+              disabled={isDownloading}
+              onClick={() => download()}
+              className="mt-16 rounded-full bg-white p-2 font-semibold leading-4 shadow-2xl transition-colors hover:bg-gray-300 disabled:cursor-wait"
+            >
+              {isDownloading ? (
+                <LoadingIcon className="h-8 w-8 text-gray-400" />
+              ) : (
+                <DownloadIcon className="h-8 w-8 text-gray-800" />
+              )}
+            </button>
+            <button
+              disabled={isPrinting}
+              onClick={() => print()}
+              className="mt-16 rounded-full bg-white p-2 font-semibold leading-4 shadow-2xl transition-colors hover:bg-gray-300 disabled:cursor-wait"
+            >
+              {isPrinting ? (
+                <LoadingIcon className="h-8 w-8 text-gray-400" />
+              ) : (
+                <PrintIcon className="h-8 w-8 text-gray-800" />
+              )}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {createPortal(
+        <iframe
+          ref={iframeRef}
+          width={1988}
+          height={3108}
+          className="hidden"
+        />,
+        document.body,
+      )}
+    </>
   )
 }
--- src/workers/png.ts
+++ src/workers/png.ts
@@ -1,10 +1,16 @@
 import { expose } from 'comlink'
 import { Resvg, initWasm } from '@resvg/resvg-wasm'
 
+let initialized = false
+
 export async function convert(svg: string) {
-  await initWasm(
-    fetch(new URL('@resvg/resvg-wasm/index_bg.wasm', import.meta.url)),
-  )
+  if (!initialized) {
+    await initWasm(
+      fetch(new URL('@resvg/resvg-wasm/index_bg.wasm', import.meta.url)),
+    )
+    initialized = true
+  }
+
   const image = new Resvg(svg).render()
   const blob = new Blob([image.asPng()], { type: 'image/png' })
   image.free()
