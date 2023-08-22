04500617ae7ab930a201db59475311645b7c80ee
Aliez <renzholy@gmail.com>
2023-08-22 20:15:30 +0900

    Update use-nfts.ts

--- src/hooks/use-nfts.ts
+++ src/hooks/use-nfts.ts
@@ -1,5 +1,5 @@
 import useSWR from 'swr'
-import { pipe, sortBy, values, map, sumBy } from 'remeda'
+import { pipe, sortBy, values, map, sumBy, uniq } from 'remeda'
 import { isAddress } from 'viem'
 import { fetchJSON } from '@/utils/fetch'
 
@@ -16,27 +16,31 @@ export function useCollections(addresses?: string[]) {
     addresses?.length ? ['collections', addresses] : null,
     async () => {
       const collections = await Promise.all(
-        addresses!.filter(isAddress).map(async (address) => {
-          try {
-            const json = await fetchJSON<{
-              collections: {
-                collection: {
-                  id: string
-                  name: string
-                  image?: string
-                  volume: { allTime: number }
-                }
-                ownership: { tokenCount: string }
-              }[]
-            }>(
-              `https://api.reservoir.tools/users/${address}/collections/v3?limit=100`,
-            )
-            return json.collections.filter(({ collection }) => collection.image)
-          } catch (err) {
-            console.error(err)
-            return []
-          }
-        }),
+        uniq(addresses!)
+          .filter(isAddress)
+          .map(async (address) => {
+            try {
+              const json = await fetchJSON<{
+                collections: {
+                  collection: {
+                    id: string
+                    name: string
+                    image?: string
+                    volume: { allTime: number }
+                  }
+                  ownership: { tokenCount: string }
+                }[]
+              }>(
+                `https://api.reservoir.tools/users/${address}/collections/v3?limit=100`,
+              )
+              return json.collections.filter(
+                ({ collection }) => collection.image,
+              )
+            } catch (err) {
+              console.error(err)
+              return []
+            }
+          }),
       )
 
       return pipe(
@@ -80,25 +84,27 @@ export function useTokens(addresses?: string[], collection?: string) {
     addresses?.length && collection ? ['tokens', addresses, collection] : null,
     async () => {
       const nfts = await Promise.all(
-        addresses!.filter(isAddress).map(async (address) => {
-          try {
-            const json = await fetchJSON<{
-              tokens: {
-                token: { contract: string; tokenId: string; image?: string }
-              }[]
-            }>(
-              `https://api.reservoir.tools/users/${address}/tokens/v7?sortBy=lastAppraisalValue&limit=200&collection=${collection}`,
-            )
-            return json.tokens.map(({ token }) => ({
-              contract: token.contract,
-              token: token.tokenId,
-              image: token.image,
-            }))
-          } catch (err) {
-            console.error(err)
-            return []
-          }
-        }),
+        uniq(addresses!)
+          .filter(isAddress)
+          .map(async (address) => {
+            try {
+              const json = await fetchJSON<{
+                tokens: {
+                  token: { contract: string; tokenId: string; image?: string }
+                }[]
+              }>(
+                `https://api.reservoir.tools/users/${address}/tokens/v7?sortBy=lastAppraisalValue&limit=200&collection=${collection}`,
+              )
+              return json.tokens.map(({ token }) => ({
+                contract: token.contract,
+                token: token.tokenId,
+                image: token.image,
+              }))
+            } catch (err) {
+              console.error(err)
+              return []
+            }
+          }),
       )
       return nfts.flatMap((tokens) => tokens)
     },
