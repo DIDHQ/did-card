1c3957c2811e3240812053ef0eca4fcc0cabc0b8
Aliez <renzholy@gmail.com>
2023-08-28 17:19:00 +0900

    Update list.ts

--- src/pages/api/nft/list.ts
+++ src/pages/api/nft/list.ts
@@ -11,6 +11,12 @@ export default async function handler(req: NextRequest) {
     return new Response(null, { status: 400 })
   }
 
+  const json = await simpleHash(addresses)
+
+  return NextResponse.json(json)
+}
+
+async function simpleHash(addresses: string): Promise<Collection[]> {
   const response = await fetch(
     `https://api.simplehash.com/api/v0/nfts/collections_by_wallets?chains=${Object.keys(
       chains,
@@ -27,36 +33,35 @@ export default async function handler(req: NextRequest) {
       },
     },
   )
+
   const json = (await response.json()) as {
     collections: {
       name: string | null
       image_url: string | null
       distinct_nfts_owned: number
       nft_ids?: string[]
-
       top_contracts?: string[]
       top_bids?: { value: number; payment_token: { symbol: string } }[]
     }[]
   }
-  return NextResponse.json(
-    sortBy(
-      json.collections.filter(
-        (collection) =>
-          collection.name && collection.image_url && collection.nft_ids,
-      ),
-      [
-        (collection) =>
-          collection.top_contracts?.[0] ===
-          'ethereum.0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb'
-            ? Infinity
-            : (collection.top_bids?.[0]?.value ?? 0) *
-              (collection.top_bids?.[0]?.payment_token.symbol.endsWith('ETH')
-                ? 1000
-                : collection.top_bids?.[0]?.payment_token.symbol.endsWith('BNB')
-                ? 100
-                : 1),
-        'desc',
-      ],
-    ) as Collection[],
-  )
+
+  return sortBy(
+    json.collections.filter(
+      (collection) =>
+        collection.name && collection.image_url && collection.nft_ids,
+    ),
+    [
+      (collection) =>
+        collection.top_contracts?.[0] ===
+        'ethereum.0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb'
+          ? Infinity
+          : (collection.top_bids?.[0]?.value ?? 0) *
+            (collection.top_bids?.[0]?.payment_token.symbol.endsWith('ETH')
+              ? 1000
+              : collection.top_bids?.[0]?.payment_token.symbol.endsWith('BNB')
+              ? 100
+              : 1),
+      'desc',
+    ],
+  ) as Collection[]
 }
