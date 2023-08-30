112d0edeed0a583b0d0277cae8758054885e7aea
Aliez <renzholy@gmail.com>
2023-08-30 11:24:34 +0900

    Update nft-collections.tsx

--- src/components/nft-collections.tsx
+++ src/components/nft-collections.tsx
@@ -21,7 +21,9 @@ export default function NftCollections(props: {
                 src="/favicon.png"
                 alt="favicon"
                 onClick={() =>
-                  props.onSelect(`${window.location.href}/favicon.png`)
+                  props.onSelect(
+                    `${new URL(window.location.href).origin}/favicon.png`,
+                  )
                 }
                 className="h-32 w-32 cursor-pointer rounded-xl object-cover ring-gray-200 transition-shadow hover:ring"
               />
