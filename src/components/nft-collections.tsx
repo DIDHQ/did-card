0128caa6807a1d436f7d2bd1e4fcffa73627711a
Aliez <renzholy@gmail.com>
2023-09-11 14:50:48 +0900

    add tako

--- src/components/nft-collections.tsx
+++ src/components/nft-collections.tsx
@@ -2,6 +2,8 @@ import { GroupedVirtuoso } from 'react-virtuoso'
 import NftToken from './nft-token'
 import { Collection } from '@/utils/type'
 
+const defaultLogos = ['/favicon.png', '/tako.png']
+
 export default function NftCollections(props: {
   collections?: Collection[]
   onSelect(image: string): void
@@ -14,19 +16,22 @@ export default function NftCollections(props: {
           <div>
             <div className="flex h-16 items-center justify-between bg-gray-100 px-6 font-semibold">
               <span className="text-gray-800">Default</span>
-              <span className="text-gray-400">1</span>
+              <span className="text-gray-400">{defaultLogos.length}</span>
             </div>
-            <div className="p-6">
-              <img
-                src="/favicon.png"
-                alt="favicon"
-                onClick={() =>
-                  props.onSelect(
-                    `${new URL(window.location.href).origin}/favicon.png`,
-                  )
-                }
-                className="h-32 w-32 cursor-pointer rounded-xl object-cover ring-gray-200 transition-shadow hover:ring"
-              />
+            <div className="flex flex-wrap gap-6 p-6">
+              {defaultLogos.map((logo) => (
+                <img
+                  key={logo}
+                  src={logo}
+                  alt={logo}
+                  onClick={() =>
+                    props.onSelect(
+                      `${new URL(window.location.href).origin}${logo}`,
+                    )
+                  }
+                  className="h-32 w-32 cursor-pointer rounded-xl object-cover ring-gray-200 transition-shadow hover:ring"
+                />
+              ))}
             </div>
           </div>
         ),
