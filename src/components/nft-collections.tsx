f266842c514e97171d23d8ee18841a95c355ab5f
Aliez <renzholy@gmail.com>
2023-11-11 10:13:19 +0800

    Update nft-collections.tsx

--- src/components/nft-collections.tsx
+++ src/components/nft-collections.tsx
@@ -2,6 +2,8 @@ import { GroupedVirtuoso } from 'react-virtuoso'
 import NftToken from './nft-token'
 import { Collection } from '@/utils/type'
 
+const defaultLogos = ['/favicon.png', '/prixpal.png']
+
 export default function NftCollections(props: {
   collections?: Collection[]
   onSelect(image: string): void
@@ -9,6 +11,31 @@ export default function NftCollections(props: {
 }) {
   return (
     <GroupedVirtuoso
+      components={{
+        Header: () => (
+          <div>
+            <div className="flex h-16 items-center justify-between bg-gray-100 px-6 font-semibold">
+              <span className="text-gray-800">Default</span>
+              <span className="text-gray-400">{defaultLogos.length}</span>
+            </div>
+            <div className="flex flex-wrap gap-6 p-6">
+              {defaultLogos.map((logo) => (
+                <img
+                  key={logo}
+                  src={logo}
+                  alt={logo}
+                  onClick={() =>
+                    props.onSelect(
+                      `${new URL(window.location.href).origin}${logo}`,
+                    )
+                  }
+                  className="h-32 w-32 cursor-pointer rounded-xl object-cover ring-gray-200 transition-shadow hover:ring"
+                />
+              ))}
+            </div>
+          </div>
+        ),
+      }}
       groupCounts={props.collections?.map(() => 1) ?? []}
       groupContent={(index) => (
         <div className="flex h-16 items-center justify-between bg-gray-100 px-6 font-semibold">
