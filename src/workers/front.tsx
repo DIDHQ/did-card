30f295e19804f3efe5325dc3a2183afa25143d01
Aliez <renzholy@gmail.com>
2024-09-29 19:27:42 +0900

    Update front.tsx

--- src/workers/front.tsx
+++ src/workers/front.tsx
@@ -124,7 +124,7 @@ export async function generateFront(did?: string, tag?: string, title?: string, 
             fontSize: 128,
             lineHeight: 1.5,
             fontWeight: 600,
-            color: '#5cb531',
+            color: '#7ff845',
           }}
         >
           {`#${tag}`}
